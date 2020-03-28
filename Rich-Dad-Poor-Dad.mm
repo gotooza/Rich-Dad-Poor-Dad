6b8e18cad2891741805f50bd8042260892b2cc10
gotooza <510hidepon@gmail.com>
2020-03-29 02:14:28 +0900

    add base files

---
+++ Rich-Dad-Poor-Dad.mm
@@ -0,0 +1,4 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1585384120330" ID="ID_306626738" MODIFIED="1585384147264" TEXT="Rich-Dad-Poor-Dad"/>
+</map>
